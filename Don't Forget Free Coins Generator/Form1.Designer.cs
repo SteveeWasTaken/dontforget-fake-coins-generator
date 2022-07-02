bbd3115179d0f60c1f5c2faf47ac5e2922bbc88a
SteveeWasTaken <tebanlopez07@gmail.com>
2022-07-02 01:51:52 -0500

    Add files via upload

---
+++ Don't Forget Free Coins Generator/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Don_t_Forget_Free_Coins_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.logoImage = new System.Windows.Forms.PictureBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.goldLabel = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.usernameInput = new System.Windows.Forms.TextBox();
+            this.undercoinLabel = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.hackLog = new System.Windows.Forms.TextBox();
+            this.undercoinInput = new System.Windows.Forms.TextBox();
+            this.goldInput = new System.Windows.Forms.TextBox();
+            this.adminCheckBox = new System.Windows.Forms.CheckBox();
+            this.noclipCheckBox = new System.Windows.Forms.CheckBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordInput = new System.Windows.Forms.TextBox();
+            this.xtraOptionsLabel = new System.Windows.Forms.Label();
+            this.banCheckBox = new System.Windows.Forms.CheckBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.ssSettingsCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoImage
+            // 
+            this.logoImage.BackColor = System.Drawing.Color.Transparent;
+            this.logoImage.Image = global::Don_t_Forget_Free_Coins_Generator.Properties.Resources.dfc;
+            resources.ApplyResources(this.logoImage, "logoImage");
+            this.logoImage.Name = "logoImage";
+            this.logoImage.TabStop = false;
+            this.logoImage.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // titleLabel
+            // 
+            resources.ApplyResources(this.titleLabel, "titleLabel");
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // goldLabel
+            // 
+            resources.ApplyResources(this.goldLabel, "goldLabel");
+            this.goldLabel.ForeColor = System.Drawing.Color.White;
+            this.goldLabel.Name = "goldLabel";
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Value = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // usernameInput
+            // 
+            resources.ApplyResources(this.usernameInput, "usernameInput");
+            this.usernameInput.Name = "usernameInput";
+            // 
+            // undercoinLabel
+            // 
+            resources.ApplyResources(this.undercoinLabel, "undercoinLabel");
+            this.undercoinLabel.ForeColor = System.Drawing.Color.White;
+            this.undercoinLabel.Name = "undercoinLabel";
+            this.undercoinLabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // usernameLabel
+            // 
+            resources.ApplyResources(this.usernameLabel, "usernameLabel");
+            this.usernameLabel.ForeColor = System.Drawing.Color.White;
+            this.usernameLabel.Name = "usernameLabel";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hackLog
+            // 
+            this.hackLog.BackColor = System.Drawing.Color.Black;
+            this.hackLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.hackLog, "hackLog");
+            this.hackLog.ForeColor = System.Drawing.Color.Lime;
+            this.hackLog.Name = "hackLog";
+            this.hackLog.ReadOnly = true;
+            this.hackLog.UseWaitCursor = true;
+            // 
+            // undercoinInput
+            // 
+            resources.ApplyResources(this.undercoinInput, "undercoinInput");
+            this.undercoinInput.Name = "undercoinInput";
+            // 
+            // goldInput
+            // 
+            resources.ApplyResources(this.goldInput, "goldInput");
+            this.goldInput.Name = "goldInput";
+            // 
+            // adminCheckBox
+            // 
+            resources.ApplyResources(this.adminCheckBox, "adminCheckBox");
+            this.adminCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.adminCheckBox.ForeColor = System.Drawing.Color.White;
+            this.adminCheckBox.Name = "adminCheckBox";
+            this.adminCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // noclipCheckBox
+            // 
+            resources.ApplyResources(this.noclipCheckBox, "noclipCheckBox");
+            this.noclipCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.noclipCheckBox.ForeColor = System.Drawing.Color.White;
+            this.noclipCheckBox.Name = "noclipCheckBox";
+            this.noclipCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // passwordLabel
+            // 
+            resources.ApplyResources(this.passwordLabel, "passwordLabel");
+            this.passwordLabel.ForeColor = System.Drawing.Color.White;
+            this.passwordLabel.Name = "passwordLabel";
+            // 
+            // passwordInput
+            // 
+            resources.ApplyResources(this.passwordInput, "passwordInput");
+            this.passwordInput.Name = "passwordInput";
+            // 
+            // xtraOptionsLabel
+            // 
+            resources.ApplyResources(this.xtraOptionsLabel, "xtraOptionsLabel");
+            this.xtraOptionsLabel.ForeColor = System.Drawing.Color.White;
+            this.xtraOptionsLabel.Name = "xtraOptionsLabel";
+            // 
+            // banCheckBox
+            // 
+            resources.ApplyResources(this.banCheckBox, "banCheckBox");
+            this.banCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.banCheckBox.ForeColor = System.Drawing.Color.White;
+            this.banCheckBox.Name = "banCheckBox";
+            this.banCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // loginButton
+            // 
+            resources.ApplyResources(this.loginButton, "loginButton");
+            this.loginButton.Name = "loginButton";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ssSettingsCheckBox
+            // 
+            resources.ApplyResources(this.ssSettingsCheckBox, "ssSettingsCheckBox");
+            this.ssSettingsCheckBox.BackColor = System.Drawing.Color.Transparent;
+            this.ssSettingsCheckBox.ForeColor = System.Drawing.Color.White;
+            this.ssSettingsCheckBox.Name = "ssSettingsCheckBox";
+            this.ssSettingsCheckBox.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.Controls.Add(this.ssSettingsCheckBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.banCheckBox);
+            this.Controls.Add(this.xtraOptionsLabel);
+            this.Controls.Add(this.passwordInput);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.noclipCheckBox);
+            this.Controls.Add(this.adminCheckBox);
+            this.Controls.Add(this.goldInput);
+            this.Controls.Add(this.undercoinInput);
+            this.Controls.Add(this.hackLog);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.undercoinLabel);
+            this.Controls.Add(this.usernameInput);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.goldLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.logoImage);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.logoImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoImage;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label goldLabel;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox usernameInput;
+        private System.Windows.Forms.Label undercoinLabel;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox hackLog;
+        private System.Windows.Forms.TextBox undercoinInput;
+        private System.Windows.Forms.TextBox goldInput;
+        private System.Windows.Forms.CheckBox adminCheckBox;
+        private System.Windows.Forms.CheckBox noclipCheckBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordInput;
+        private System.Windows.Forms.Label xtraOptionsLabel;
+        private System.Windows.Forms.CheckBox banCheckBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.CheckBox ssSettingsCheckBox;
+    }
+}
+
---
+++ Don't Forget Free Coins Generator/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Don_t_Forget_Free_Coins_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            // Console.WriteLine("start wait timer");
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                // Console.WriteLine("stop wait timer");
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logoImage.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        void hideUI()
+        {
+            usernameInput.Visible = false;
+            undercoinInput.Visible = false;
+            goldInput.Visible = false;
+            passwordInput.Visible = false;
+            goldLabel.Visible = false;
+            passwordLabel.Visible = false;
+            undercoinLabel.Visible = false;
+            usernameLabel.Visible = false;
+            adminCheckBox.Visible = false;
+            noclipCheckBox.Visible = false;
+            titleLabel.Visible = false;
+            logoImage.Visible = false;
+            banCheckBox.Visible = false;
+            ssSettingsCheckBox.Visible = false;
+            xtraOptionsLabel.Visible = false;
+            startButton.Visible = false;
+        }
+
+        void showUI()
+        {
+            usernameInput.Visible = true;
+            undercoinInput.Visible = true;
+            goldInput.Visible = true;
+            passwordInput.Visible = true;
+            goldLabel.Visible = true;
+            passwordLabel.Visible = true;
+            undercoinLabel.Visible = true;
+            usernameLabel.Visible = true;
+            adminCheckBox.Visible = true;
+            noclipCheckBox.Visible = true;
+            titleLabel.Visible = true;
+            logoImage.Visible = true;
+            banCheckBox.Visible = true;
+            ssSettingsCheckBox.Visible = true;
+            xtraOptionsLabel.Visible = true;
+            startButton.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gold = 0;
+            int undercoins = 0;
+            var works = true;
+            try
+            {
+                gold = int.Parse(goldInput.Text);
+            }
+            catch (Exception)
+            {
+                works = false;
+                MessageBox.Show("Please input a number as Gold.", "Error");
+            }
+            try
+            {
+                undercoins = int.Parse(undercoinInput.Text);
+            }
+            catch (Exception)
+            {
+                works = false;
+                MessageBox.Show("Please input a number as UnderCoins.", "Error");
+            }
+            if (works == true)
+            {
+                hideUI();
+                progressBar1.Visible = true;
+                hackLog.Visible = true;
+                var theuser = usernameInput.Text;
+                string userpass = "";
+                for (int i = 0; i < passwordInput.TextLength; i++)
+                {
+                    userpass += "*";
+                }
+                hackLog.Text = "Hacking the database...";
+                progressBar1.Value = 5;
+                wait(6000);
+                hackLog.Text = "Hacking the database [DONE]" + Environment.NewLine + "Downloading Information...";
+                progressBar1.Value = 8;
+                wait(5500);
+                hackLog.Text = "Hacking the database [DONE]" + Environment.NewLine + "Downloading Information [DONE]" + Environment.NewLine + "Accessing DONTFORGET CONNECTED Servers with user " + theuser + " And Password " + userpass + "...";
+                progressBar1.Value = 15;
+                wait(2000);
+                hackLog.Text = "Hacking the database [DONE]" + Environment.NewLine + "Downloading Information [DONE]" + Environment.NewLine + "Accessing DONTFORGET CONNECTED Servers with user " + theuser + " And Password " + userpass + " [DONE]" + Environment.NewLine + "Sending pair request to DONTFORGET CONNECTED Servers...";
+                progressBar1.Value = 45;
+                wait(15000);
+                hackLog.Text = "Hacking the database [DONE]" + Environment.NewLine + "Downloading Information [DONE]" + Environment.NewLine + "Accessing DONTFORGET CONNECTED Servers with user " + theuser + " And Password " + userpass + " [DONE]" + Environment.NewLine + "Sending pair request to DONTFORGET CONNECTED Servers [DONE]" + Environment.NewLine + "Uploading data...";
+                progressBar1.Value = 70;
+                wait(7000);
+                hackLog.Text = "Hacking the database [DONE]" + Environment.NewLine + "Downloading Information [DONE]" + Environment.NewLine + "Accessing DONTFORGET CONNECTED Servers with user " + theuser + " And Password " + userpass + " [DONE]" + Environment.NewLine + "Sending pair request to DONTFORGET CONNECTED Servers [DONE]" + Environment.NewLine + "Uploading data [FAILED]" + Environment.NewLine + "Captcha Verification is needed. A website has opened, follow instructions to verify your account and recieve free gold hack.";
+                progressBar1.Value = 95;
+                System.Diagnostics.Process.Start("https://bit.ly/3bIxKjY");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void usernameInput_TextChanged(object sender, EventArgs e)
+        {
+            setButtonVisibility();
+        }
+
+        private void passwordInput_TextChanged(object sender, EventArgs e)
+        {
+            setButtonVisibility();
+        }
+
+        private void setButtonVisibility()
+        {
+            if ((usernameInput.Text != String.Empty) && (passwordInput.Text != String.Empty))
+            {
+                loginButton.Enabled = true;
+            }
+            else
+            {
+                loginButton.Enabled = false;
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool loginCheck = true;
+            if (usernameInput.TextLength == 0)
+            {
+                MessageBox.Show("You haven't given a Username.", "Error");
+                loginCheck = false;
+            }
+            if (passwordInput.TextLength == 0)
+            {
+                MessageBox.Show("You haven't given a Password.", "Error");
+                loginCheck = false;
+            }
+            if (usernameInput.TextLength <= 3)
+            {
+                MessageBox.Show("Your username is too short.", "Error");
+                loginCheck = false;
+            }
+            if (passwordInput.TextLength <= 7)
+            {
+                MessageBox.Show("Your password is too short.", "Error");
+                loginCheck = false;
+            }
+            if (loginCheck)
+            {
+                startButton.Visible = true;
+                startButton.Enabled = true;
+                goldInput.Visible = true;
+                undercoinInput.Visible = true;
+                goldLabel.Visible = true;
+                undercoinLabel.Visible = true;
+                xtraOptionsLabel.Visible = true;
+                noclipCheckBox.Visible = true;
+                adminCheckBox.Visible = true;
+                banCheckBox.Visible = true;
+                ssSettingsCheckBox.Visible = true;
+                usernameInput.Visible = false;
+                usernameLabel.Visible = false;
+                passwordInput.Visible = false;
+                passwordLabel.Visible = false;
+                loginButton.Visible = false;
+                MessageBox.Show("Login Successful.", "Success");
+            }
+        }
+    }
+}
